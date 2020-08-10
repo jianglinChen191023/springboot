9c6dba886c1d7d537761a6093ff3996dc5c73d80
jianglinChen191023 <1912623646@qq.com>
2020-08-10 17:18:19 +0800

    hello

---
+++ atguigu/chapter1/src/main/java/com/atguigu/springboot/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author CHEN Jianglin
+ * @description 全局引导
+ * @date 2020/8/10 / 16:12
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
---
+++ atguigu/chapter1/src/test/java/com.atguigu.springboot/DomeApplicationTests.java
@@ -0,0 +1,18 @@
+package com.atguigu.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author CHEN Jianglin
+ * @description
+ * @date 2020/8/10 / 16:12
+ */
+@SpringBootApplication
+public class DomeApplicationTests {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
